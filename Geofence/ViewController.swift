bc47f5c7907eeaa8221f54e0703c70cdc75f249b
Rayan Gan <rayangan@mobius-digital.com>
2019-09-28 12:36:07 +0800

    Initial Commit

---
+++ Geofence/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geofence
+//
+//  Created by Rayan Gan on 28/09/2019.
+//  Copyright © 2019 Settle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
